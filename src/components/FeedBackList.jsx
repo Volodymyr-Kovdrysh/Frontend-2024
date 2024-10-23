e04f4cd083d028e99186fe10b81bc0b9ca5aa186
VVKo <v.v.kovdrysh@gmail.com>
2024-10-24 01:22:17 +0300

    create Spinner and change context
    Signed-off-by: VVKo <v.v.kovdrysh@gmail.com>

--- src/components/FeedBackList.jsx
+++ src/components/FeedBackList.jsx
@@ -2,13 +2,16 @@ import {motion} from 'framer-motion';
 import FeedBackItem from "./FeedBackItem.jsx";
 import {useContext} from "react";
 import FeedbackContext from "../context/FeedbackContext.jsx";
+import Spinner from "../shared/Spinner.jsx";
 
 const FeedBackList = () => {
-    const {feedbacks} = useContext(FeedbackContext);
-    if (!feedbacks || feedbacks.length === 0) {
+    const {feedbacks, isLoading} = useContext(FeedbackContext);
+    if (!isLoading && (!feedbacks || feedbacks.length === 0)) {
         return <p>Повідомлень немає</p>
     }
-    return (
+
+
+    return isLoading ? <Spinner/>: (
         <div className="feedback-list">
             {feedbacks.map((item) => (
                 <motion.div
--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
 import FeedbackData from "../data/FeedbackData.js";
 import {v4 as uuidv4} from "uuid";
 
@@ -7,11 +7,32 @@ const FeedbackContext = createContext()
 export const FeedbackProvider = ({ children }) => {
 
 
-    const [feedbacks, setFeedbacks] = useState(FeedbackData);
+    const [feedbacks, setFeedbacks] = useState([]);
+    const [isLoading, setIsloading] = useState(true)
+
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false,
     })
+
+    useEffect(()=>{
+        fetchFeedbacks()
+            .then(data => {
+                setFeedbacks(data);
+                setIsloading(false)
+            })
+            .catch(error => {
+                console.error('Error fetching feedbacks:', error);
+            });
+    }, [])
+
+    const fetchFeedbacks = async () => {
+        const response = await fetch('http://localhost:3000/feedbacks');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
+    };
 
     const deleteFeedback = (id) => {
         if(window.confirm('Ви впевнені, що хочете видалити цей важливий відгук??')
@@ -20,17 +41,39 @@ export const FeedbackProvider = ({ children }) => {
         }
     }
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedbacks([newFeedback, ...feedbacks])
+    const addFeedback = async (newFeedback) => {
+        setIsloading(true)
+        const response = await fetch('http://localhost:3000/feedbacks?_sort=id&_order=asc', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback)
+        })
+        const data = await response.json()
+
+        // newFeedback.id = uuidv4()
+        setFeedbacks([data, ...feedbacks])
+        setIsloading(false)
     }
 
-    const updateFeedback = (id, updItem) => {
-        setFeedbacks(feedbacks.map(item => item.id === id ? {...item, ...updItem } : item))
+    const updateFeedback = async (id, updItem) => {
+        setIsloading(true)
+        const response = await fetch(`http://localhost:3000/feedbacks/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updItem)
+        })
+        const data = await response.json()
+        setFeedbacks(feedbacks.map(item => item.id === id ? {...item, ...data } : item))
+        // setFeedbacks(feedbacks.map(item => item.id === id ? {...item, ...updItem } : item))
         setFeedbackEdit({
             item: {},
             edit: false,
         })
+        setIsloading(false)
     }
 
     const editFeedback = (item) => {
@@ -45,7 +88,8 @@ export const FeedbackProvider = ({ children }) => {
         addFeedback,
         updateFeedback,
         editFeedback,
-        feedbackEdit
+        feedbackEdit,
+        isLoading
     }}>
         {children}
     </FeedbackContext.Provider>;
